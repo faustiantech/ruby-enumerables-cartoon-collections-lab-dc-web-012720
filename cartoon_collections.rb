ac650e863bb9e4121cdb246e3d71438323052325
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-21 23:39:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,5 @@ def find_the_cheese(source)# code an argument here
   if source&cheese_types == []
     return nil
     else 
-      return source&cheese_types[0]]
+      return source&cheese_types[0]
 end
