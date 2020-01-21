34f5a9663f2f5f269cb1cd39710163b597d2dd04
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-21 23:15:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(source)# code an argument here
   source.each_with_index {|b,i| puts "#{i+1}. #{b}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(source)# code an argument here
   # Your code here
+  source.map()
 end
 
 def long_planeteer_calls# code an argument here
