625766706a12d545166695e0a7be3f4d8251da66
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-21 23:26:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ end
 
 def long_planeteer_calls(source)# code an argument here
   # Your code here
-  source.reduce do |word|
-    word.length > 4 ? return true : nil 
+  source.each do |word|
+    if word.length > 4 ? return true
   end
   return false
 end
